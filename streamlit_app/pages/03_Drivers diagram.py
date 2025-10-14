7f4c15a659e0d7c12c83e4fcd1502a79220fa678
ludovicapiccioli <ludovica.piccioli@wur.nl>
2025-10-14 16:38:54 +0200

    Update 03_Drivers diagram.py

--- streamlit_app/pages/03_Drivers diagram.py
+++ streamlit_app/pages/03_Drivers diagram.py
@@ -1,4 +1,5 @@
 import streamlit as st
 
 st.title("Drivers diagram")
-st.write("In progress. This page will show the drivers diagram.")
+st.write("To be completed (in progress). This page will show the drivers diagram.")
+
