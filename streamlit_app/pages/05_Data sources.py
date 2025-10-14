9dcc30439b07172c835d5eae34a589d0a25aedee
ludovicapiccioli <ludovica.piccioli@wur.nl>
2025-10-14 16:46:43 +0200

    Update 05_Data sources.py

---
+++ streamlit_app/pages/05_Data sources.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("Data sources")
+st.write("Kerncijfers wijken en buurten 2024, https://www.cbs.nl/nl-nl/cijfers/detail/85984NED")
+st.write("Nabijheid voorzieningen; afstand locatie, wijk- en buurtcijfers 2022, https://www.cbs.nl/nl-nl/cijfers/detail/85560NED")
+st.write("Gezondheid per wijk en buurt; 2012/2016/2020/2022 (indeling 2022), https://data.overheid.nl/en/dataset/42936-gezondheid-per-wijk-en-buurt--2012-2016-2020-2022--indeling-2022-")
