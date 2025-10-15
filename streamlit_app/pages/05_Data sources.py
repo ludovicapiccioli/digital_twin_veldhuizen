aa42ac959a55ed6e5405eb199275dbb3eb0b7e5e
ludovicapiccioli <ludovica.piccioli@wur.nl>
2025-10-15 14:27:05 +0200

    Update 05_Data sources.py

--- streamlit_app/pages/05_Data sources.py
+++ streamlit_app/pages/05_Data sources.py
@@ -4,4 +4,5 @@ st.title("Data sources")
 st.write("Kerncijfers wijken en buurten 2024, https://www.cbs.nl/nl-nl/cijfers/detail/85984NED")
 st.write("Nabijheid voorzieningen; afstand locatie, wijk- en buurtcijfers 2022, https://www.cbs.nl/nl-nl/cijfers/detail/85560NED")
 st.write("Gezondheid per wijk en buurt; 2012/2016/2020/2022 (indeling 2022), https://data.overheid.nl/en/dataset/42936-gezondheid-per-wijk-en-buurt--2012-2016-2020-2022--indeling-2022-")
-st.write("Code availability: https://github.com/ludovicapiccioli/digital_twin_veldhuizen/tree/main/streamlit_app/data"
+st.write("Code availability: https://github.com/ludovicapiccioli/digital_twin_veldhuizen/tree/main/streamlit_app/data")
+
