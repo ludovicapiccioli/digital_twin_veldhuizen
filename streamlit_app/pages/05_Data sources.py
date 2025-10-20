b2816946942153111d182cb5a8d47400ef69c7a4
ludovicapiccioli <ludovica.piccioli@wur.nl>
2025-10-20 20:05:37 +0200

    Update 05_Data sources.py

--- streamlit_app/pages/05_Data sources.py
+++ streamlit_app/pages/05_Data sources.py
@@ -1,9 +1,10 @@
 import streamlit as st
 
 st.title("Data sources")
-st.write("Kerncijfers wijken en buurten 2024, https://www.cbs.nl/nl-nl/cijfers/detail/85984NED")
+st.write("Centraal Bureau voor de Statistiek. (2025, March 27). Kerncijfers wijken en buurten 2024. Centraal Bureau Voor de Statistiek. https://www.cbs.nl/nl-nl/cijfers/detail/85984NED ")
 st.write("Nabijheid voorzieningen; afstand locatie, wijk- en buurtcijfers 2022, https://www.cbs.nl/nl-nl/cijfers/detail/85560NED")
-st.write("Gezondheid per wijk en buurt; 2012/2016/2020/2022 (indeling 2022), https://data.overheid.nl/en/dataset/42936-gezondheid-per-wijk-en-buurt--2012-2016-2020-2022--indeling-2022-")
+st.write("Rijksinstituut voor Volksgezondheid en Milieu (RIVM). (2025, December 10). Gezondheid per wijk en buurt; 2012/2016/2020/2022 (indeling 2022). Overheid. https://data.overheid.nl/en/dataset/42936-gezondheid-per-wijk-en-buurt--2012-2016-2020-2022--indeling-2022- ")
 st.write("Code availability: https://github.com/ludovicapiccioli/digital_twin_veldhuizen/tree/main/streamlit_app")
+
 
 
