63ee76936267d5c69fe533b980e733eaf777ec9c
ludovicapiccioli <ludovica.piccioli@wur.nl>
2025-10-20 21:53:14 +0200

    Update 05_Data sources.py

--- streamlit_app/pages/05_Data sources.py
+++ streamlit_app/pages/05_Data sources.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 st.title("Sources")
 st.write("Centraal Bureau voor de Statistiek. (2025, March 27). Kerncijfers wijken en buurten 2024. Centraal Bureau Voor de Statistiek. https://www.cbs.nl/nl-nl/cijfers/detail/85984NED ")
-st.write("Centraal Bureau voor de Statistiek. (2025b, March 28). Nabijheid voorzieningen; afstand locatie, wijk- en buurtcijfers 2022. Centraal Bureau Voor de Statistiek. https://www.cbs.nl/nl-nl/cijfers/detail/85560NED")
+st.write("Centraal Bureau voor de Statistiek. (2025, March 28). Nabijheid voorzieningen; afstand locatie, wijk- en buurtcijfers 2022. Centraal Bureau Voor de Statistiek. https://www.cbs.nl/nl-nl/cijfers/detail/85560NED")
 st.write("Rijksinstituut voor Volksgezondheid en Milieu (RIVM). (2025, December 10). Gezondheid per wijk en buurt; 2012/2016/2020/2022 (indeling 2022). Overheid. https://data.overheid.nl/en/dataset/42936-gezondheid-per-wijk-en-buurt--2012-2016-2020-2022--indeling-2022- ")
+
 
