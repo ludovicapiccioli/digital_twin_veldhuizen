3492e93f1ce90f4ef407d4508c832e8b9545003d
ludovicapiccioli <ludovica.piccioli@wur.nl>
2025-10-21 20:49:55 +0200

    Update 04_Scenarios.py

--- streamlit_app/pages/04_Scenarios.py
+++ streamlit_app/pages/04_Scenarios.py
@@ -311,7 +311,7 @@ svg = f'''
         marker-end="url(#arrowGreen2)"/>
   <text x="755" y="330" class="cap" font-size="18" fill="#19a974">x2</text>
 
-  <path d="M570,480 C780,440 720,420 760,360"
+  <path d="M570,480 C780,440 720,420 768,360"
         fill="none" stroke="#19a974" stroke-width="{ARROW_W_X1}"
         marker-end="url(#arrowGreen1)"/>
   <text x="780" y="430" class="cap" font-size="18" fill="#19a974">x1</text>
@@ -351,6 +351,7 @@ g = go.Figure(go.Indicator(
 ))
 g.update_layout(height=240, margin=dict(l=10, r=10, t=40, b=10), template="plotly_white")
 st.plotly_chart(g, use_container_width=True)
+
 
 
 
